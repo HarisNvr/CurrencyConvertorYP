a7e86a6724f3d865d90f66d73c588b93b4f4bfba
Haro Mikhaylov <www.haris@bk.ru>
2024-12-25 13:03:05 +0300

    #2: feat: add view func to update courses

--- backend/CurrencyConvertor/urls.py
+++ backend/CurrencyConvertor/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 
+from CurrencyConvertor.settings import HASH_URL
 from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('rates/', views.currency_rates_view, name='currency_rates'),
+    path(f'{HASH_URL}', views.update_course_db, name='update_course_db')
 ]
