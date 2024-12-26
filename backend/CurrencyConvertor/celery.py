854035a12f4727ff2a83079d2e40df51cefc0f47
Anton <patrin_toxa@mail.ru>
2024-12-26 16:14:34 +0500

    feat: add Celery and celery beat

---
+++ backend/CurrencyConvertor/celery.py
@@ -0,0 +1,20 @@
+import os
+
+from celery import Celery
+from celery.beat import crontab
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CurrencyConvertor.settings")
+app = Celery(
+    "celery",
+    broker_connection_retry=False,
+    broker_connection_retry_on_startup=True,
+)
+app.config_from_object("django.conf:settings", namespace="CELERY")
+app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    "update_rate_every_single_hour": {
+        "task": "api.tasks.update_course_db_task",
+        "schedule": crontab(minute=10, hour="*/1"),
+    },
+}
