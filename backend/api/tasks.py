231e103cc1fdbbaa555495885d39ca76ceb6b552
Anton <patrin_toxa@mail.ru>
2024-12-26 17:24:32 +0500

    fix: add docstring and switch function

--- backend/api/tasks.py
+++ backend/api/tasks.py
@@ -1,7 +1,18 @@
 from CurrencyConvertor.celery import app
-from api.views import update_course_db
+
+from backend.api.utils import get_and_save_all_rates
 
 
 @app.task
 def update_course_db_task() -> None:
-    update_course_db()
+    """
+    Updates the exchange rates in the database.
+
+    This task function is triggered by Celery beat and calls the
+    `get_and_save_all_rates` function to fetch and save the latest exchange
+    rates. If an error occurs during the update process, a task failure is
+    raised.
+
+    :return: None
+    """
+    get_and_save_all_rates()
