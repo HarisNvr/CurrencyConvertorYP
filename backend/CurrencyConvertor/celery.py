15c9efee52e0a00c8e6e05ef1297bbb4ab1d791f
Haro Mikhaylov <www.haris@bk.ru>
2024-12-27 16:39:18 +0300

    #8: fix: alter celery.py

--- backend/CurrencyConvertor/celery.py
+++ backend/CurrencyConvertor/celery.py
@@ -1,9 +1,9 @@
-import os
+from os import environ
 
 from celery import Celery
 from celery.beat import crontab
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "CurrencyConvertor.settings")
+environ.setdefault("DJANGO_SETTINGS_MODULE", "CurrencyConvertor.settings")
 app = Celery(
     "celery",
     broker_connection_retry=False,
@@ -16,6 +16,6 @@ app.conf.enable_utc = True
 app.conf.beat_schedule = {
     "update_rate_every_single_day": {
         "task": "api.tasks.update_course_db_task",
-        "schedule": crontab(minute=40, hour=0, day_of_week="*"),
+        "schedule": crontab(minute='30', hour='0', day_of_week="*"),
     },
 }
